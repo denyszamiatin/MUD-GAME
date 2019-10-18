ccddee64bee96af6e3e98e32204fe9caa2be7530
inuzhyn <inuzhyn@luxoft.com>
2019-10-18 16:30:47 +0300

    Replace wis with end

--- main.py
+++ main.py
@@ -31,9 +31,9 @@ def is_valid_email(string: str) -> bool:
 
 # Races parameters (strength = stn, wisdom = wis, agility = agl, intellect = inl)
 race_stats = {
-    'human': {'stn':3, 'wis':3, 'agl':3, 'inl':3},
-    'elf': {'stn':2, 'wis':4, 'agl':4, 'inl':2},
-    'dworf': {'stn':4, 'wis':2, 'agl':3, 'inl':3},
+    'human': {'stn': 3, 'end': 3, 'agl': 3, 'inl': 3},
+    'elf': {'stn': 2, 'end': 2, 'agl': 4, 'inl': 4},
+    'dworf': {'stn': 4, 'end': 4, 'agl': 2, 'inl': 2},
 }
 
 
