f3ff7cf2bbe1d8993b5931cd0718ad87ad9552c0
inuzhyn <inuzhyn@luxoft.com>
2019-10-20 12:53:28 +0300

    add registration file add dummy generate_name()

---
+++ registration.py
@@ -0,0 +1,9 @@
+
+def generate_name():
+
+    """TODO - implement generation of name:
+    Имя начинается с большой буквы
+    Длина имени - 5-9 символов
+    Имя не должно содержать более 2-х гласных или согласных подряд"""
+    return "DummyName"
+
