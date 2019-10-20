b556ef6331d2ffe17b374251f78af17fee16979d
inuzhyn <inuzhyn@luxoft.com>
2019-10-20 13:03:54 +0300

    add dummy choose_race() move is_valid_email() to registration

--- main.py
+++ main.py
@@ -1,32 +1,7 @@
-import re
 
 
-email_pattern = re.compile(r"^.{1,64}@[A-Za-z0-9\._-]+\.[a-zA-Z]*$")
-EMAIL_MAX_LENGTH = 320
 
 
-def is_valid_email(string: str) -> bool:
-    """Return True if string is valid email
-
-    TODO - Not implemented:
-    name: - ! # $ % & ‘ * + — / =? ^ _ ` { | } ~ a-zA-Z0-9-.
-    name: . ! # $ % & ‘ * + — / =? ^ _ ` { | } ~  cant be first or last.
-    name: . ! # $ % & ‘ * + — / =? ^ _ ` { | } ~  cant appear twice or more in row
-    name: only one .
-    domain: at least 2 [a-zA-Z] after last dot
-
-    >>> is_valid_email("A"*(EMAIL_MAX_LENGTH + 1))
-    False
-    >>> is_valid_email("@example.org")
-    False
-    >>> is_valid_email('a'*65 + "@example.org")
-    False
-    >>> is_valid_email("a@ABCabc-123.org")
-    True
-    >>> is_valid_email("a@a.b.com")
-    True
-    """
-    return len(string) <= EMAIL_MAX_LENGTH and bool(email_pattern.match(string))
 
 
 # Races parameters (strength = stn, Endurance = end, agility = agl, intellect = inl)
--- registration.py
+++ registration.py
@@ -1,3 +1,5 @@
+import re
+
 
 def generate_name():
 
@@ -7,3 +9,40 @@ def generate_name():
     Имя не должно содержать более 2-х гласных или согласных подряд"""
     return "DummyName"
 
+
+def choose_race():
+
+    """TODO - implement race choice :
+    При регистрации новый игрок должен выбрать расу, к которой будет принадлежать.
+    Необходимо реализовать функцию,
+    которая позволит пользователю выбрать расу и произвести валидацию выбора"""
+
+    return "elf"
+
+
+email_pattern = re.compile(r"^.{1,64}@[A-Za-z0-9\._-]+\.[a-zA-Z]*$")
+EMAIL_MAX_LENGTH = 320
+
+
+def is_valid_email(string: str) -> bool:
+    """Return True if string is valid email
+
+    TODO - Not implemented:
+    name: - ! # $ % & ‘ * + — / =? ^ _ ` { | } ~ a-zA-Z0-9-.
+    name: . ! # $ % & ‘ * + — / =? ^ _ ` { | } ~  cant be first or last.
+    name: . ! # $ % & ‘ * + — / =? ^ _ ` { | } ~  cant appear twice or more in row
+    name: only one .
+    domain: at least 2 [a-zA-Z] after last dot
+
+    >>> is_valid_email("A"*(EMAIL_MAX_LENGTH + 1))
+    False
+    >>> is_valid_email("@example.org")
+    False
+    >>> is_valid_email('a'*65 + "@example.org")
+    False
+    >>> is_valid_email("a@ABCabc-123.org")
+    True
+    >>> is_valid_email("a@a.b.com")
+    True
+    """
+    return len(string) <= EMAIL_MAX_LENGTH and bool(email_pattern.match(string))
